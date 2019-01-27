8e045958cafed4fbc58cf3237579105c12ae0ffb
Yves Riedener <git@reecube.com>
2019-01-27 18:39:54 +0100

    implemented firebase authentication

--- app/src/main/java/com/reecube/sensedoor_prototype/HomeFragment.java
+++ app/src/main/java/com/reecube/sensedoor_prototype/HomeFragment.java
@@ -9,13 +9,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class HomeFragment extends Fragment {
 
+    private FirebaseAuth mAuth;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        TextView homeText = view.findViewById(R.id.home_text);
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        homeText.setText(user != null ? user.getUid() : "DISCONNECTED!");
     }
 }
--- app/src/main/java/com/reecube/sensedoor_prototype/MainActivity.java
+++ app/src/main/java/com/reecube/sensedoor_prototype/MainActivity.java
@@ -1,5 +1,6 @@
 package com.reecube.sensedoor_prototype;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,12 +9,19 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Toast;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActionBar toolbar;
 
     private Fragment[] fragments;
+
+    private FirebaseAuth mAuth;
 
     private static final int FRAGMENT_HOME = 0;
     private static final int FRAGMENT_DASHBOARD = 1;
@@ -54,12 +62,46 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private static final int RC_SIGN_IN = 9001;
+
+    private void startSignIn() {
+        // Build FirebaseUI sign in intent. For documentation on this operation and all
+        // possible customization see: https://github.com/firebase/firebaseui-android
+        Intent intent = AuthUI.getInstance().createSignInIntentBuilder()
+                .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                .setAvailableProviders(Collections.singletonList(
+                        new AuthUI.IdpConfig.EmailBuilder().build()))
+                .setLogo(R.mipmap.ic_launcher)
+                .build();
+
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // Sign in succeeded
+                updateUI(mAuth.getCurrentUser());
+            } else {
+                // Sign in failed
+                Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         toolbar = getSupportActionBar();
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         fragments = new Fragment[4];
 
@@ -76,4 +118,25 @@ public class MainActivity extends AppCompatActivity {
         loadFragment(FRAGMENT_HOME);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        updateUI(mAuth.getCurrentUser());
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+
+            // TODO: implement this
+        } else {
+
+            startSignIn();
+        }
+    }
+
+    private void signOut() {
+        AuthUI.getInstance().signOut(this);
+        updateUI(null);
+    }
 }

553792dd8acb369a6dd0cdab4e7b3d80bfdc334e
Yves Riedener <git@reecube.com>
2019-01-27 17:46:59 +0100

    implemented navigation fragments

--- app/src/main/java/com/reecube/sensedoor_prototype/MainActivity.java
+++ app/src/main/java/com/reecube/sensedoor_prototype/MainActivity.java
@@ -3,13 +3,29 @@ package com.reecube.sensedoor_prototype;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private ActionBar toolbar;
+
+    private Fragment[] fragments;
+
+    private static final int FRAGMENT_HOME = 0;
+    private static final int FRAGMENT_DASHBOARD = 1;
+    private static final int FRAGMENT_MATRIX = 2;
+    private static final int FRAGMENT_JOYSTICK = 3;
+
+    private void loadFragment(int index) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragments[index]);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,16 +34,20 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    toolbar.setTitle(R.string.title_home);
+                    loadFragment(FRAGMENT_HOME);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    toolbar.setTitle(R.string.title_dashboard);
+                    loadFragment(FRAGMENT_DASHBOARD);
                     return true;
                 case R.id.navigation_matrix:
-                    mTextMessage.setText(R.string.title_matrix);
+                    toolbar.setTitle(R.string.title_matrix);
+                    loadFragment(FRAGMENT_MATRIX);
                     return true;
                 case R.id.navigation_joystick:
-                    mTextMessage.setText(R.string.title_joystick);
+                    toolbar.setTitle(R.string.title_joystick);
+                    loadFragment(FRAGMENT_JOYSTICK);
                     return true;
             }
             return false;
@@ -39,9 +59,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = findViewById(R.id.message);
+        toolbar = getSupportActionBar();
+
+        fragments = new Fragment[]{
+                new HomeFragment(),
+                new DashboardFragment(),
+                new MatrixFragment(),
+                new JoystickFragment(),
+        };
+
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        toolbar.setTitle(R.string.title_home);
+
+        loadFragment(FRAGMENT_HOME);
     }
 
 }
---
+++ app/src/main/java/com/reecube/sensedoor_prototype/MatrixFragment.java
@@ -0,0 +1,27 @@
+package com.reecube.sensedoor_prototype;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MatrixFragment extends Fragment {
+
+    public MatrixFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_matrix, container, false);
+    }
+}
